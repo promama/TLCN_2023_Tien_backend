43702e382b4744c3380dfd26860f7434fd2d00fa
promama <promama@users.noreply.github.com>
2023-09-26 10:03:25 +0700

    create user

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -18,68 +18,79 @@ module.exports.test = async (req, res) => {
 };
 
 module.exports.createAccount = (req, res) => {
-  if (req.body.password === req.body.repassword) {
-    User.find({ email: req.body.email })
-      .exec()
-      .then((user) => {
-        //check if email already existed
-        if (user.length > 0) {
-          res.status(409).json({
-            success: false,
-            message: "email existed",
-          });
-        } else {
-          //hash password
-          bcrypt.hash(req.body.password, 10, (err, hashed) => {
-            if (err) {
-              res.status(500).json({
-                success: false,
-                message: err,
-              });
-            } else {
-              //create a new user with email and password being hash
-              const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hashed,
-              });
-
-              //create cart with userId
-              const cart = new Cart({
-                _id: new mongoose.Types.ObjectId(),
-                userId: user._id,
-              });
-
-              cart.save().then((result) => {
-                console.log(result);
-              });
-
-              user
-                .save()
-                .then((result) => {
-                  console.log(result);
-                  res.status(201).json({
-                    success: true,
-                    data: {
-                      email: user.email,
-                    },
-                    message: "create user success!",
-                  });
-                })
-                .catch((err) => {
-                  res.status(500).json({
-                    success: false,
-                    message: err,
-                  });
-                });
-            }
-          });
-        }
-      });
-  } else {
+  if (
+    req.body.email == "" ||
+    req.body.password == "" ||
+    req.body.repassword == ""
+  ) {
     res.status(400).json({
       success: false,
-      message: "password and repassword not match!",
+      message: "please fill all field",
     });
+  } else {
+    if (req.body.password === req.body.repassword) {
+      User.find({ email: req.body.email })
+        .exec()
+        .then((user) => {
+          //check if email already existed
+          if (user.length > 0) {
+            res.status(409).json({
+              success: false,
+              message: "email existed",
+            });
+          } else {
+            //hash password
+            bcrypt.hash(req.body.password, 10, (err, hashed) => {
+              if (err) {
+                res.status(500).json({
+                  success: false,
+                  message: err,
+                });
+              } else {
+                //create a new user with email and password being hash
+                const user = new User({
+                  _id: new mongoose.Types.ObjectId(),
+                  email: req.body.email,
+                  password: hashed,
+                });
+
+                //create cart with userId
+                const cart = new Cart({
+                  _id: new mongoose.Types.ObjectId(),
+                  userId: user._id,
+                });
+
+                cart.save().then((result) => {
+                  console.log(result);
+                });
+
+                user
+                  .save()
+                  .then((result) => {
+                    console.log(result);
+                    res.status(201).json({
+                      success: true,
+                      data: {
+                        email: user.email,
+                      },
+                      message: "create user success!",
+                    });
+                  })
+                  .catch((err) => {
+                    res.status(500).json({
+                      success: false,
+                      message: "email format is not correct",
+                    });
+                  });
+              }
+            });
+          }
+        });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "password and repassword not match!",
+      });
+    }
   }
 };
